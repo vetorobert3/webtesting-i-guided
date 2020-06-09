bb6c1eac08d026cc6c6de402a7e7e25e74494a64
Veto Ramirez <vetorobert3@gmail.com>
2020-06-08 20:21:30 -0700

    need to work on test @ 1:15

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,35 @@
 const { add } = require('./calculator.js');
 
-// test away!
+// AAA
+// Arrange
+// Act
+// Assert
+
+describe('calculator functions', () => {
+  describe('test add', () => {
+    let a = 2;
+    let b = 2;
+    let expected = 4;
+    let result = -1
+    it('returns sum of two numbers', () => {
+      result = add(a, b);
+      expect(result).toBe(expected);
+
+      a = 2;
+      b = 3;
+      expected = 5;
+      result = add(a, b);
+      expect(result).toBe(expected);
+    });
+
+    it('returns the value passed when only one arguement is provided', () => {
+      a = null;
+      b = 3;
+      expected = 3;
+      result = add(b);
+      expect(result).toBe(expected);
+    })
+
+    it.todo('returns the sum ');
+  })
+})
